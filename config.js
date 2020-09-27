b9858f1e852639f79acba5425bae0c85f3304398
Nnaemeka <N.nnaemeka@gmail.com>
2020-09-27 12:48:06 -0500

    added new packages

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: 'http://hf-food.austin.utexas.edu/foodpro/longmenu.aspx?sName=University+Housing+and+Dining&locationNum=03&locationName=Kinsolving+Dining&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=09%2f27%2f2020&mealName=Breakfast'
+}
